37cb6d5eab185ee67d957e6f013c9eb87b20dbab
Nikhil Singh <nikhilsingh498@gmail.com>
2019-08-07 03:57:34 +0530

    Add index page with styles and scripts

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+	let id = [
+		"home",
+		"sponsors",
+		"speakers",
+		"tech-used",
+		"contact-us"
+	]
+	let colors = [
+		"#FE524D",
+		"#4E96FC",
+		"#FFC334",
+		"#34AD6D",
+		"#4E96FC"
+	]
+
+	id.forEach( e => {
+		document.getElementById(e).addEventListener( 'click', (event) => {
+			document.getElementById(e).style.borderBottom = "6px solid " +  colors[Number(event.target.getAttribute("color"))]
+			document.getElementById(e).childNodes[1].style.color = colors[Number(event.target.getAttribute("color"))]
+			id.forEach( ele => {
+				if (ele != e){
+					document.getElementById(ele).style.borderBottom = "6px solid transparent"; 
+					document.getElementById(ele).childNodes[1].style.color = "gray"
+				}
+			})
+		})
+	})
+}
